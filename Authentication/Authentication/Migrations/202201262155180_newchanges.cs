3a678dca878157b0f5ad911fe453824be3c1786f
Alex Bennett <alexbennett12@live.com>
2022-01-26 16:16:49 -0600

    saving
    saved

---
+++ Authentication/Authentication/Migrations/202201262155180_newchanges.cs
@@ -0,0 +1,17 @@
+﻿namespace Authentication.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class newchanges : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- ClassExampleNET4/ClassExampleNET4/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClassExampleNET4.MvcApplication" Language="C#" %>
