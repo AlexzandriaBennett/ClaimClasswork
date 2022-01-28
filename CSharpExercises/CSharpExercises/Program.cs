651e03373a449eae01db2cc2b08a1da81357b776
Alex Bennett <alexbennett12@live.com>
2022-01-27 23:11:28 -0600

    saving
    saved

--- CSharpExercises/CSharpExercises/Program.cs
+++ CSharpExercises/CSharpExercises/Program.cs
@@ -1,10 +1,182 @@
-﻿class Program
+﻿using System.Linq;
+
+class Program
 {
     static void Main(string[] args)
     {
-       
+
         //Console.WriteLine("This is a test");
-        Exercise1();
+        //Exercise1();
+        //Exercise2();
+        //Exercise3();
+        //Exercise4();
+        //Exercise5();
+
+        Exercice6();
+
+    }
+
+
+    // Summary:
+    // Write a program and continuously ask the user to enter different names, until the user presses Enter 
+    // (without supplying a name). Depending on the number of names provided, display a message based on the above pattern.
+   
+    private static void Exercice6()
+    {
+        Console.WriteLine("Please enter names who like your post and press enter \nwhen finished leave empty and press enter");
+        var input = Console.ReadLine();
+        var listOfNames = new List<string>();
+        while (input.Length > 0)
+        {
+            listOfNames.Add(input);
+
+            
+        }
+        else
+        {
+            foreach(var name in listOfNames)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        
+        
+
+    }
+
+    // Summary:
+    // Write a program and ask the user to enter an English word. Count the number of vowels 
+    // (a, e, o, u, i) in the word. So, if the user enters "inadequate", the program should display 
+    // 6 on the console. Make sure the program calculates the number of vowels irrespective of the 
+    // casing of the word (eg "Inadequate", "inadequate" and "INADEQUATE" all include 6 vowels).
+
+    private static void Exercise5()
+    {
+        Console.WriteLine("Please enter an English word");
+        var input = Console.ReadLine().ToCharArray();
+        char[] unallowableValues = { 'a', 'e', 'i', 'o', 'u', 'A','E','I','O','U'};
+        var instance = 0;
+
+        foreach (char value in unallowableValues)
+        {
+
+            foreach (char letter in input)
+            {
+                if (letter == value)
+                {
+                    instance++;
+                }
+            }
+           
+        }
+        Console.WriteLine(instance + " vowels");
+        
+   
+
+    }
+
+    //Summary:
+    // Write a program and ask the user to enter a few words separated by a space.
+    // Use the words to create a variable name with PascalCase. 
+    // For example, if the user types: "number of students", display "NumberOfStudents". 
+    // Make sure that the program is not dependent on the input. 
+    // So, if the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents".
+    private static void Exercise4()
+    {
+        Console.WriteLine("Please enter a few words separated by a space");
+        var input = Console.ReadLine();
+
+        if (String.IsNullOrWhiteSpace(input))
+        {
+            Console.WriteLine("Error");
+            return;
+        }
+
+        var variableName = "";
+        foreach (var word in input.Split(' '))
+        {
+            var wordWithPascalCase = char.ToUpper(word[0]) + word.ToLower().Substring(1);
+            variableName += wordWithPascalCase;
+        }
+
+        Console.WriteLine(variableName);
+
+
+
+
+        //var splitted = input.Split(' ');
+        //foreach (var line in splitted)
+        //{
+        //    line.ToUpper();
+        //    Console.WriteLine(line);
+        //}
+    }
+
+    //Summary:
+    //Write a program and ask the user to enter a time value in the 24-hour time format (e.g. 19:00). 
+    //A valid time should be between 00:00 and 23:59.
+    //If the time is valid, display "Ok"; otherwise, display "Invalid Time". 
+    //If the user doesn't provide any values, consider it as invalid time.
+    private static void Exercise3()
+    {
+        Console.WriteLine("Please enter a time value in the 24-hour time format (e.g. 19:00)");
+        var input = Console.ReadLine();
+        bool isWhiteSpace = string.IsNullOrWhiteSpace(input);
+        if (isWhiteSpace == true)
+        {
+            Console.WriteLine("invalid input");
+            Exercise3();
+        }
+        var date = DateTime.TryParse(input, out DateTime result);
+        if (date == true)
+        {
+            Console.WriteLine("Ok");
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
+        }
+        
+
+    }
+
+
+
+
+    //Summary:
+    //Write a program and ask the user to enter a few numbers separated by a hyphen.
+    //If the user simply presses Enter, without supplying an input, exit immediately;
+    //otherwise, check to see if there are duplicates.
+    //If so, display "Duplicate" on the console.
+    private static void Exercise2()
+    {
+        Console.WriteLine("Please enter numbers seperated by a hyphen(-) symbol");
+        var input = Console.ReadLine();
+        var numbers = new List<int>();
+        bool isWhiteSpace = string.IsNullOrWhiteSpace(input);
+        if (isWhiteSpace == true)
+        {
+            Environment.Exit(0);
+        }
+        else
+        {
+            var splittedInput = input.Split('-');
+            foreach(var item in splittedInput)
+            {
+                numbers.Add(int.Parse(item));
+            }
+            if(numbers.Count != numbers.Distinct().Count())
+            {
+                Console.WriteLine("duplicate");
+            }
+            else
+            {
+                Console.WriteLine("all good!");
+            }
+            
+        }
+        
     }
 
 
@@ -39,22 +211,6 @@
         var message = isConsecutive ? "Consecutive" : "Not Consecutive";
         Console.WriteLine(message);
 
-        //for (int i = 0; i < newInput.Length; i++)
-        //{
-        //    if (newInput[i] > newInput[i + 1])
-        //    }
-        
-        //var isSorted = true;
-        //for (int i = 0; i < newinput.Count - 1; i++)
-        //{
-        //    if (newinput[i] > newinput[i + 1])
-        //    {
-        //        isSorted = true;
-        //        break;
-        //    }
-        //}
-
-        //Console.WriteLine(list);
     }
 }
 
---
+++ jan27/jan27/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jan27.MvcApplication" Language="C#" %>
